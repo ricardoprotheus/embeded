cea9cef4eb11398d048f8a8ae5a053bfd4f18632
telso <51102850+telso@users.noreply.github.com>
2020-07-07 22:50:59 -0300

    ajuste para producao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EmbedqryComponent } from './embedqry/embedqry.component';
 
-
-const routes: Routes = [
-  { path: 'embedqry', component: EmbedqryComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
